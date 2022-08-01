6148a41c5f5cdf3e286fd008fd4eaff1b1761e13
Nicholas Lukasevich <n.luka784@gmail.com>
2022-08-01 18:28:02 -0400

    Add triangle coloring

--- main.js
+++ main.js
@@ -6,19 +6,31 @@
     throw new Error("WebGL not supported");
   }
 
-  // triangle
-  const vertices = new Float32Array([0, 1, 0, /**/ 1, -1, 0, /**/ -1, -1, 0]);
+  const triangle = {
+    vertices: new Float32Array([0, 1, 0, /**/ 1, -1, 0, /**/ -1, -1, 0]),
+    colors: new Float32Array([1, 0, 0, /**/ 0, 1, 0, /**/ 0, 0, 1]),
+  };
 
-  const buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, triangle.vertices, gl.STATIC_DRAW);
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, triangle.colors, gl.STATIC_DRAW);
 
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(
     vertexShader,
     `
+    precision mediump float;
+
     attribute vec3 position;
+    attribute vec3 color;
+    varying vec3 vColor;
+
     void main() {
+      vColor = color;
       gl_Position = vec4(position, 1);
     }
     `
@@ -29,8 +41,12 @@
   gl.shaderSource(
     fragmentShader,
     `
+    precision mediump float;
+
+    varying vec3 vColor;
+
     void main() {
-      gl_FragColor = vec4(1, 0, 0, 1);
+      gl_FragColor = vec4(vColor, 1);
     }
     `
   );
@@ -43,8 +59,14 @@
 
   const positionLoc = gl.getAttribLocation(program, "position");
   gl.enableVertexAttribArray(positionLoc);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
 
+  const colorLoc = gl.getAttribLocation(program, "color");
+  gl.enableVertexAttribArray(colorLoc);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 0, 0);
+
   gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
+  gl.drawArrays(gl.TRIANGLES, 0, triangle.vertices.length / 3);
 })();
